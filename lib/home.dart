cfc7557408b6c9a565c87689f7c7f09fe558078c
Harshit kumar <harshitrudrapur@gmail.com>
2020-04-05 14:44:54 +0530

    base layout

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("testing"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:digitaleventpass/home.dart';
 
 void main() => runApp(MaterialApp(
-
+   initialRoute: "/home",
+   routes: {
+     "/home" : (context)=> home(),
+   },
 ));
+
+
