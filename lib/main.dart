8f84efcb3b0df9e0943b8dbbf92e3cee16b07a24
sahma61 <sahmaanwar61@gmail.com>
2020-04-05 14:06:43 +0530

    a slight change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-
+  home: MyApp(),
 ));
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Digital Event Pass",style: TextStyle(color: Colors.black)),
+    ),
+    body: Center(child: Text("Hello")),
+    bottomNavigationBar: BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle),
+        title: Text("User")),
+        BottomNavigationBarItem(icon: Icon(Icons.list),
+        title: Text("List")
+        )
+      ]
+    )
+  );
+}
+}
