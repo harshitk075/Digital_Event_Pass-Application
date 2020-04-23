0357d2809c17d28bbbad06ccbff10bd8064fdd46
Harshit kumar <harshitrudrapur@gmail.com>
2020-04-23 23:12:46 +0530

    syncing remote

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ import 'package:digitaleventpass/pages/home.dart';
 import 'package:digitaleventpass/pages/organizerpage.dart';
 import 'package:digitaleventpass/pages/user/userpage.dart';
 
+
 void main() => runApp(MaterialApp(
-   initialRoute: "/login",
+   initialRoute: "/",
    routes: {
      "/" : (context)=> home(),
      "/organizerPage" : (context)=> organizerPage(),
