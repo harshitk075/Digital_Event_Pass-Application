f2f994a37708fc22c4c421a16bc803676ac6be29
Harshit kumar <harshitrudrapur@gmail.com>
2021-01-12 22:43:47 +0530

    fixed final bugs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -57,7 +57,7 @@ class _homeState extends State<home> {
   void initState() {
     super.initState();
     checkLogin();
-    fetchProfileData();
+    //fetchProfileData();
   }
 
   @override
@@ -189,20 +189,6 @@ class _homeState extends State<home> {
           home.seturl(document.data['OrgimgURL']);
       });
     }
-  }
-
-  Future<void> fetchProfileData() async {
-    final _firestore = Firestore.instance;
-    //print(widget.mUid);
-    var document =
-        await _firestore.collection('OrganizerContainer').document(_uid).get();
-    print(document.data);
-    home.setUsername(document.data['OrgName']);
-    home.setnumber(document.data['OrgContactNumber']);
-    home.setgender(document.data['OrgGender']);
-    home.setemail(document.data['OrgmailId']);
-    home.seturl(document.data['OrgimgURL']);
-    //print(document.data['OrgmailId']);
   }
 }
 
